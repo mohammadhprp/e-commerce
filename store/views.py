2483fc0649069860168252dd9952c5ea5ce6a4c2
blackjohnrose <18.mamali@gmail.com>
2021-03-05 19:51:23 +0330

    add order to context

--- store/views.py
+++ store/views.py
@@ -14,7 +14,17 @@ def store(request):
     return render(request, 'store/Store.html', context)
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+
+    context = {
+        'items': items,
+        'order': order
+    }
     return render(request, 'store/Cart.html', context)
 
 def checkout(request):
