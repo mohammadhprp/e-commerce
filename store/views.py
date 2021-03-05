218495c1b17d0c4120dd6166e0ca2ea086000752
blackjohnrose <18.mamali@gmail.com>
2021-03-05 20:07:17 +0330

    add func item and total

--- store/views.py
+++ store/views.py
@@ -20,6 +20,10 @@ def cart(request):
         items = order.orderitem_set.all()
     else:
         items = []
+        order = {
+            'get_cart_total': 0,
+            'get_cart_items': 0
+        }
 
     context = {
         'items': items,
