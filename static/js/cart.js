3b7874d99261d9fc07e0f4d39ed62c8ee99dde0a
blackjohnrose <18.mamali@gmail.com>
2021-03-09 19:51:54 +0330

    create fun for create json file

--- static/js/cart.js
+++ static/js/cart.js
@@ -11,8 +11,27 @@ for (var i = 0; i < updateBtns.length; i++) {
             console.log('User is not authenticated')
         }
         else {
-        console.log('User is authenticated, sending data...')
+            updateUserOrder(productId, action)
         }
     })
+}
+function updateUserOrder(productId, action){
+    console.log('User is authenticated, sending data...')
 
+    var url = '/update_item/'
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json',
+            'X-CSRFToken': csrftoken
+        },
+        body:JSON.stringify({'productId': productId, 'action': action})
+    })
+    .then ((response) => {
+        return response.json()
+    })
+    .then ((data) => {
+        console.log('data: ', data)
+    })
 }
