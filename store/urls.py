3041d6996fb35e8e8aaff6105a2b1e4db69086ec
blackjohnrose <18.mamali@gmail.com>
2021-03-09 19:49:14 +0330

    add path link updata_item

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import store, cart, checkout
+from .views import store, cart, checkout, updateItem
 
 
 urlpatterns = [
     path('', store, name='store'),
     path('cart/', cart, name='cart'),
     path('checkout/', checkout, name='checkout'),
+
+    path('update_item/', updateItem, name='update-item'),
 
 ]
