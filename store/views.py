5d746c8562a8a53704c781f35bea201f58381a8d
blackjohnrose <18.mamali@gmail.com>
2021-03-09 19:49:58 +0330

    add fun updateItem for add produc for json file

--- store/views.py
+++ store/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 from .models import (
     Customer,
     Product,
@@ -48,3 +50,31 @@ def checkout(request):
         'order': order
     }
     return render(request, 'store/Checkout.html', context)
+
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print('action:', action)
+    print('productId:', productId)
+
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity  + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity  - 1)
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+        #30:00
+    return JsonResponse('Item was added', safe=False)
