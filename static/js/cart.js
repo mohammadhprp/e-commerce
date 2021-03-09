e6b723dfb981b32b3882d42c74366e287b70994c
blackjohnrose <18.mamali@gmail.com>
2021-03-09 18:14:31 +0330

    add loop for add product id & action and if for user

---
+++ static/js/cart.js
@@ -0,0 +1,18 @@
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for (var i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function(){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId', productId, 'action', action)
+
+        console.log('User: ', user)
+        if(user === 'AnonymousUser'){
+            console.log('User is not authenticated')
+        }
+        else {
+        console.log('User is authenticated, sending data...')
+        }
+    })
+
+}
