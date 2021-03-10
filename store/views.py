37648bc83186113062ee3f15af0a92021479967c
blackjohnrose <18.mamali@gmail.com>
2021-03-10 23:41:45 +0330

    add orider item to order

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
+import datetime
 from .models import (
     Customer,
     Product,
@@ -102,5 +103,30 @@ def updateItem(request):
     return JsonResponse('Item was added', safe=False)
 
 def processOrder(request):
-    print('Data', request.body)
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == float(order.get_cart_total):
+            order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                    customer = customer,
+                    order = order,
+                    address = data['shipping']['address'],
+                    city = data['shipping']['city'],
+                    state = data['shipping']['state'],
+                    zipcode = data['shipping']['zipcode'],
+                )
+
+    else:
+        print('user is not logged in... ')
+
     return JsonResponse('pyment complete', safe=False)
